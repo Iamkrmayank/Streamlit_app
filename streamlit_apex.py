a7704eff98c27b182558a9c64cb15893d54e90f8
Kumar Mayank <103871423+Iamkrmayank@users.noreply.github.com>
2024-02-07 02:45:37 +0530

    Algorithm Visualization with Streamlit

---
+++ streamlit_apex.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Dictionary mapping algorithm names to image URLs
+algorithm_images = {
+    'Decision Tree': '/Users/kumarmayank/Desktop/SL/DT.png',
+    'Random Forest': '/Users/kumarmayank/Desktop/SL/RF.png',
+    'Linear Regression': '/Users/kumarmayank/Desktop/SL/LR.png',
+    'Extra Trees':'/Users/kumarmayank/Desktop/SL/ET.png',
+    'Gradient Boosting':'/Users/kumarmayank/Desktop/SL/GB.png',
+    'K Nearest Neigbour':'/Users/kumarmayank/Desktop/SL/Knn.png',
+    'Mean Model':'/Users/kumarmayank/Desktop/SL/Mean.png',
+    'Median Model':'/Users/kumarmayank/Desktop/SL/Median.png',
+    'XgBoost':'/Users/kumarmayank/Desktop/SL/Xgboost.png',
+    'HistGBM':'/Users/kumarmayank/Desktop/SL/HistGBM.png'
+    # Add more algorithms as needed
+}
+
+# Streamlit app
+st.title('Algorithm Visualization with Streamlit')
+
+# Dropdown for selecting the algorithm
+selected_algorithm = st.selectbox('Select Algorithm:', list(algorithm_images.keys()))
+
+# Display the selected algorithm's image
+if selected_algorithm in algorithm_images:
+    st.image(algorithm_images[selected_algorithm], caption=f'{selected_algorithm} Visualization', use_column_width=True)
+
